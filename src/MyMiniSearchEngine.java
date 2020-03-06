4bb7e6a1bbfb7a4c88f21cdfbd5b4a70ea29e857
MyMasterArtoria <MyMasterArtoria@192.168.0.6>
2020-03-05 21:42:56 -0800

    phase1

--- src/MyMiniSearchEngine.java
+++ src/MyMiniSearchEngine.java
@@ -18,16 +18,45 @@ public class MyMiniSearchEngine {
     // each item in the List is considered a document.
     // assume documents only contain alphabetical words separated by white spaces.
     private void index(List<String> texts) {
-        //homework
+        for(int i=0;i<texts.size();i++) {
+    	String[] words=texts.get(i).split(" ");
+    		for(int j=0;j<words.length;j++) {
+    			System.out.println(words[j]);
+    			/*if(!indexes.containsKey(words[j])){
+    				List<List<Integer>> list = new ArrayList<List<Integer>>();
+    				List<Integer> inlist= new ArrayList<>();
+    				inlist.add(i);
+    				list.add(inlist);
+    				indexes.put(words[j], list);
+    			}
+    			if(indexes.containsKey(words[j])){
+    				List<Integer> inlist= new ArrayList<>();
+    				inlist.add(i);
+    				List<List<Integer>> list = new ArrayList<List<Integer>>();
+    				list=indexes.get(words[j]);
+    				list.add(inlist);
+    				indexes.replace(words[j], list);
+    			}*/
+    		}
+    	}
+ }
+        
+        
+                
     	
-    	
-    }
+    
 
     // search(key) return all the document ids where the given key phrase appears.
     // key phrase can have one or two words in English alphabetic characters.
     // return an empty list if search() finds no match in all documents.
     public List<Integer> search(String keyPhrase) {
-        // homework
-        return new ArrayList<>(); // place holder
+        List<Integer> docId= new ArrayList<>();
+        for(int i=0;i<indexes.get(keyPhrase).size();i++)
+        {
+        	for(int j=0;j<indexes.get(keyPhrase).get(i).size();j++)
+        	{docId.add(indexes.get(keyPhrase).get(i).get(j));
+        	}
+        }
+        return docId; // place holder
     }
 }
